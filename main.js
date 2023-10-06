a5c0fb9c1973e23cb813fad6d8b2889c50517e4f
kalaiarasan04 <kalaiarasan0t@gmail.com>
2023-10-06 21:28:48 +0530

    update

---
+++ main.js
@@ -0,0 +1,18 @@
+let user=parseInt(prompt("Enter a number"));
+
+let ans=()=>{
+	
+	if(user%2==0){
+		
+		document.write("Given number is even");
+		
+	}
+	
+	else{
+		
+		document.write("Given number is odd");
+		
+	}
+	
+}
+ans()
